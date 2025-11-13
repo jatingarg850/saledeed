9368a88ef8041f9e652a57d156b76a51585ca9a0
jatingarg850 <jatingarg850@gmail.com>
2025-11-13 19:27:18 +0530

    DScdscD

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'via.placeholder.com'],
   },
+  webpack: (config, { dev, isServer }) => {
+    // Disable CSS minification to avoid cssnano parsing errors
+    if (!dev && !isServer) {
+      config.optimization.minimize = false;
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
